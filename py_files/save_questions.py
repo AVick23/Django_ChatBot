a6c1b8e9f658427dfde036bb596dd539dc432f9d
AVick <vikulov2811@gmail.com>
2024-05-19 11:20:27 +0300

    Обновил пути для файлов

--- py_files/save_questions.py
+++ py_files/save_questions.py
@@ -1,9 +1,4 @@
 import sqlite3
-import spacy
-from rank_bm25 import BM25Okapi
-
-# Загрузка языковой модели Spacy
-nlp = spacy.load('ru_core_news_lg')
 
 db_path = "university.sqlite"
 
@@ -14,44 +9,6 @@ def save_question_to_db(question_text, user_id):
     cursor.execute("INSERT INTO questions (user_id, question) VALUES (?, ?)", (user_id, question_text))
     conn.commit()
     conn.close()
-
-def search_similar_questions(question_text):
-    similar_questions = []
-
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT DISTINCT question_id FROM answers")
-    answered_question_ids = [row[0] for row in cursor.fetchall()]
-
-    # Создание списка текстов вопросов для обучения BM25
-    questions_corpus = []
-    for question_id in answered_question_ids:
-        cursor.execute("SELECT question FROM questions WHERE id = ?", (question_id,))
-        answered_question_text = cursor.fetchone()[0]
-        questions_corpus.append(answered_question_text)
-
-    # Инициализация и обучение модели BM25
-    bm25 = BM25Okapi(questions_corpus)
-
-    # Применение языковой модели к входному вопросу
-    question_doc = nlp(question_text)
-
-    # Вычисление весов BM25 для каждого вопроса в базе данных
-    scores = bm25.get_scores(question_doc.text.lower().split())
-
-    # Сортировка идентификаторов вопросов по убыванию весов
-    ranked_question_ids = [x for _, x in sorted(zip(scores, answered_question_ids), reverse=True)]
-
-    # Получение текста похожих вопросов в соответствии с ранжированными идентификаторами
-    for question_id in ranked_question_ids:
-        cursor.execute("SELECT question FROM questions WHERE id = ?", (question_id,))
-        answered_question_text = cursor.fetchone()[0]
-        similar_questions.append(answered_question_text)
-
-    conn.close()
-
-    return similar_questions
 
 def get_answer_for_question(question_text):
     with sqlite3.connect(db_path) as con:
--- py_files/searh_directions.py
+++ py_files/searh_directions.py
@@ -1,6 +1,50 @@
 import sqlite3
+import spacy
+from rank_bm25 import BM25Okapi
+
+# Загрузка языковой модели Spacy
+nlp = spacy.load('ru_core_news_lg')
+
 db_path = "university.sqlite"
 
+
+def search_similar_questions(question_text):
+    similar_questions = []
+
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT DISTINCT question_id FROM answers")
+    answered_question_ids = [row[0] for row in cursor.fetchall()]
+
+    # Создание списка текстов вопросов для обучения BM25
+    questions_corpus = []
+    for question_id in answered_question_ids:
+        cursor.execute("SELECT question FROM questions WHERE id = ?", (question_id,))
+        answered_question_text = cursor.fetchone()[0]
+        questions_corpus.append(answered_question_text)
+
+    # Инициализация и обучение модели BM25
+    bm25 = BM25Okapi(questions_corpus)
+
+    # Применение языковой модели к входному вопросу
+    question_doc = nlp(question_text)
+
+    # Вычисление весов BM25 для каждого вопроса в базе данных
+    scores = bm25.get_scores(question_doc.text.lower().split())
+
+    # Сортировка идентификаторов вопросов по убыванию весов
+    ranked_question_ids = [x for _, x in sorted(zip(scores, answered_question_ids), reverse=True)]
+
+    # Получение текста похожих вопросов в соответствии с ранжированными идентификаторами
+    for question_id in ranked_question_ids:
+        cursor.execute("SELECT question FROM questions WHERE id = ?", (question_id,))
+        answered_question_text = cursor.fetchone()[0]
+        similar_questions.append(answered_question_text)
+
+    conn.close()
+
+    return similar_questions
 
 # Здесь вы можете вызвать функцию поиска направлений, которую вы определили ранее,
 def search_directions_strict(area, subjects, form):
